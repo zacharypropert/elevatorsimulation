9f950a3f712305f223220edd2e3879c4acca6e7e
zacharypropert <zacharypropert@users.noreply.github.com>
2016-02-23 18:03:04 -0500

    Update eCar.java

--- eCar.java
+++ eCar.java
@@ -1,6 +1,31 @@
-/* 
-*
-* Class E-Car.
-* Designs the elevator car and the base of how it operates.
-*
-*/
+/**
+ * Write a description of class ECar here.
+ * 
+ * Jeremy & Troy
+ * February 20, 2016
+ */
+public class ECar
+{
+    private int Idle; //not sure about this one
+    private UpList upList;
+    private DownList downList;
+    private SinkList sinkList;
+    private InCarList incarList;
+    private int floor;
+    
+    /**
+     * Constructor for objects of class ECar
+     * Starts the car at floor 1
+     */
+    public ECar(UpList u, DownLiist d, SinkList s, InCarList i)
+    {
+        Idle = 1;
+        upList = u;
+        downList = d;
+        sinkList = s;
+        incarList = i;
+        floor = 1;
+        
+    }
+
+}
