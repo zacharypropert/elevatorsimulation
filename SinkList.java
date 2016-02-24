e4846b95c72c5a480aaa5194c5bf0f35f2eacc26
zacharypropert <zacharypropert@users.noreply.github.com>
2016-02-23 19:57:47 -0500

    Update SinkList.java

--- SinkList.java
+++ SinkList.java
@@ -5,7 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class SinkList
+public class SinkList extends PList
 {
     // instance variables - replace the example below with your own
     private int x;
@@ -19,15 +19,4 @@ public class SinkList
         x = 0;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
