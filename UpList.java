ea2925735fc4d1299d9317da7d25faf4ad4089f1
zacharypropert <zacharypropert@users.noreply.github.com>
2016-02-23 19:58:22 -0500

    Update UpList.java

--- UpList.java
+++ UpList.java
@@ -5,29 +5,25 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class UpList
+public class UpList extends PList
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
     /**
      * Constructor for objects of class UpList
      */
     public UpList()
     {
-        // initialise instance variables
-        x = 0;
+        //
+    }
+    
+    /**
+     * passengers going UP
+     */
+    public void createUpList()
+    {
+        //
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+
 }
