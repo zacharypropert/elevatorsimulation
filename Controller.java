1be6a47c40c3480cbd7080b80728a8e97ba2d5aa
zacharypropert <zacharypropert@users.noreply.github.com>
2016-03-01 18:20:01 -0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -14,7 +14,7 @@ public class Controller
     private DownList downList;
     private SinkList sinkList;
     private InCarList incarList;
-    private int maxFloor; //EDIT EDIT
+    private int maxFloor; 
     private PassengerSource source;
     private ECar car;
     private int tick;
