acc0b9a2d18d1a08e38847d3ee6d5fe748dfaa31
Troy <tpastirko@student.cccnj.edu>
2016-03-03 17:49:48 -0500

    Update eCar.java

--- eCar.java
+++ eCar.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class ECar here.
  * 
@@ -8,58 +8,41 @@
 public class ECar
 {
     private int Idle; //not sure about this one
-    private UpList upList;
-    private DownList downList;
-    private SinkList sinkList;
-    private InCarList incarList;
+    private int direction;
+    private UpList u;
+    private DownList d;
+    private SinkList s;
+    private InCarList i;
+    private int maxFloor;
     private int floor;
-    private int maxfloor;
+    
     /**
      * Constructor for objects of class ECar
      * Starts the car at floor 1
      */
-    public ECar(UpList u, DownList d, SinkList s, InCarList i, int floors)
+    public ECar(UpList u, DownList d, SinkList s, InCarList i, int m)
     {
-        upList = u;
-        downList = d;
-        sinkList = s;
-        incarList = i;
+        Idle = 1;
+        direction = 1; //e-car starts going up initially
+        this.u = u;
+        this.d = d;
+        this.s = s;
+        this.i = i;
+        maxFloor = m;
         floor = 1;
-        maxFloor = floors;
         
     }
+    
     public void act()
     {
-        //for the first floor the elevator can only go up
-        if(floor == 1  && u.size() > 0 && i.size() == 0){
-            direction = 1;
-            while(floor <= maxFloor && u.size() > 0){
-                for(int x = 0; x<u.size(); x++){
-                    if(u.get(x).getStart==floor)
-                        i.add(u.remove(x));
-                    if(i.get(y).getDestination() == floor)
-                        i.removeAtFloor;
-                }
-                floor++;
-            }
-            direction = 0;
+        for(int x = floor - direction; x <= maxFloor && x > 0; x=x+direction){
+            
         }
+             
         
-        //when the elevator finishes its first round up it will begin to go down
-        //it will pick up passengers on its way down and drop them off as well
-        if(direction = 0 && floor != 1){
-            direction = -1;
-            while(floor >= Idle){
-                if(d.size()>0){
-                    for(int y = 0;y<d.size(); y++){
-                        if(d.get(y).getStart() == floor)
-                            i.add(d.remove(y));
-                        if(i.get(y).getDestination() == floor)
-                            i.removeAtFloor;
-                    }
-                }
-                floor = floor - 1
-            }
-        }
+        
+    }
+    
+    
 
 }
