dda67b9136c08a7d4164112e30db276dd17a6820
ConnorKeenan <whitekeys1234@gmail.com>
2016-03-03 18:16:27 -0500

    Update PassengerSource.java

--- PassengerSource.java
+++ PassengerSource.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 /**
  * Generates Passengers with randomized attributes, and inserts into UpList, DownList collections. Randomly generates the starting location and the destination..
  *
@@ -8,86 +7,70 @@ import java.util.*;
  */
 public class PassengerSource
 {
-
-    private UpList upList;  
-    private DownList downList;  
+    private UpList upList;
+    private DownList downList;
     Random rgen = new Random();
     private int maxFloor;
     private int rgenFloor;
-    private Clock clock;  //JEREMY ADDED!
-    
+    private Clock clock; //JEREMY ADDED!
     // private Passenger newPassenger;
-
     /**
      * Constructor for objects of class PassengerSource
      */
-    public PassengerSource(UpList upList, DownList downList, int maxFloor, Clock clock)  //JEREMY!
+    public PassengerSource(UpList upList, DownList downList, int maxFloor, Clock clock) //JEREMY!
     {
         //upList=new ArrayList<>();
         //downList=new ArrayList<>();
         //createPassenger();
-        this.clock= clock;  //JEREMY
-        rgenFloor = rgen.nextInt(3);    
-    }   
+        this.clock= clock; //JEREMY
+        rgenFloor = rgen.nextInt(3);
+        this.upList = upList;
+        this.downList = downList;
+    }
 
     /**
      *Creates and places passenger
      */
-
     public void createPassenger()
     {
-        
         int halfMax = maxFloor / 2;
         int randomInt = halfMax + maxFloor;
         int start = rgen.nextInt(randomInt - (halfMax / 4) + 1);
-       // rgenFloor = rgen.nextInt(randomInt);  
+        // rgenFloor = rgen.nextInt(randomInt);
         int end = rgen.nextInt(randomInt + 1);
-        
         if(start > maxFloor)
             start = 1;
-            
         if(end > maxFloor)
             end = 1;
-            
         while(start==end)
         {
             end = rgenFloor;
-            rgenFloor = rgen.nextInt(3);  
+            rgenFloor = rgen.nextInt(3);
         }
-
         Passenger newPassenger = new Passenger(start, end, clock.getTick());//JEREMY
-
         if(newPassenger.getStart() < newPassenger.getDestination())
         {
             upList.addPassenger(newPassenger);
-        }else  
+        }else
         {
             downList.addPassenger(newPassenger);
         }
     }
 
-    
     public void test()
     {
-        
         System.out.println("Up List");
-        
-           upList.display();
-
+        upList.display();
         System.out.println();
         System.out.println();
         System.out.println("Down List");
-        
-            downList.display();
-
+        downList.display();
     }
-    
+
     public void act(int tick)
     {
         createPassenger();
         createPassenger();
         test();
-        
-    }
-
+}
 }
