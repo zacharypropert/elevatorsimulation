2d9f7607609b3343196c2221e3324ffdb1438f7d
sweissman <sweissman@cccnj.edu>
2016-03-03 17:57:08 -0500

    Added files via upload

---
+++ Clock.java
@@ -0,0 +1,38 @@
+
+/**
+ * Creates a master Clock that maintains the time while Elevator is running.
+ * 
+ * David Hoadley
+ * 3/1/16
+**/
+public class Clock
+{
+    // instance variables - replace the example below with your own
+    private int tick;
+
+    /**
+     * Constructor for objects of class Clock
+     */
+    public Clock()
+    {
+        tick = 0; 
+    }
+
+    /**
+     * getTick will return the tick when it is called.
+     **/
+    public int getTick()
+    {
+        // Returns the current tick
+        return tick;
+    }
+    
+    /**
+     * This method will add one to tick.
+     **/
+    public void incrementTick()
+    {
+        //Adds one tick to global tick
+        tick++;
+    }
+}
