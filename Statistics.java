57c90abaef119dd8f2d171ab5d80e581fbd0b81e
ConnorKeenan <whitekeys1234@gmail.com>
2016-03-24 16:59:47 -0400

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -18,7 +18,7 @@ public class Statistics
     /**
      */
     public void avgWait()
-    {
+    {        
         int wait = 0;
         int temp = 0;
         int totalWaitTime = 0;
@@ -32,10 +32,45 @@ public class Statistics
         }
         for(int i : waitList)
         {
-            temp = wait;
-            totalWaitTime = temp + wait;
+            temp = i;
+            totalWaitTime = temp + i;
             avgWait = totalWaitTime/countPassenger;
         }
         System.out.println("The average wait time for all passengers was " +avgWait);
+    }
+
+    public void longestWait()
+    {        
+        int wait = 0;
+        int temp = 0;
+        int totalWaitTime = 0;        
+        for(Passenger p : sinkList.getCloneList())
+        {
+            wait = p.getStartTick() - p.getExitTick();
+            if(wait>temp)
+            {
+                temp = wait;                
+            }
+        }
+
+        System.out.println("The longest wait was " + temp);
+    }
+
+    public void numberOfPassengers()
+    {
+        int countPassenger = 0;
+        for(Passenger p : sinkList.getCloneList())
+        {
+            countPassenger++;
+        }
+
+        System.out.println("There were " + countPassenger + " passengers");
+    }
+
+    public void fullReport()
+    {
+        avgWait();
+        longestWait();
+        numberOfPassengers();
     }
 }
