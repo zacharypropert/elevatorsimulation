c0c944cc8f7312e21b24f6863ed5d9981a9def23
ConnorKeenan <whitekeys1234@gmail.com>
2016-03-30 19:21:34 -0400

    Update InCarList.java

--- InCarList.java
+++ InCarList.java
@@ -12,7 +12,7 @@ public class InCarList extends PList
      * Constructor for objects of class InCarList
      */
     public InCarList(Clock c)                                       //CKnote - Takes in Clock from PList
-    
+
     {
         //
         super(c);
@@ -24,14 +24,37 @@ public class InCarList extends PList
         for(Passenger p : other) {
             p.setEntryTick(clock.getTick());
             pList.add(p);
-            
+
         }
     }
-    
+
     public boolean isEmpty(){                                       //CKnote - If pList is empty, return true
         if(pList.isEmpty() == true)
             return true;
         return false;
     }
 
+    public int checkRequest(int currentFloor, int direction)       //CKnote - Returns the 
+    {
+        int floor = currentFloor;
+        int closest = 0;
+        //loops through entire list
+        for (int x =0;x<pList.size();x++) {
+
+            while(direction==1)                                     //CKnote - While going up
+            {                                                       //CKnote - Closest should change if destination is between eCar and previous mark
+                if (pList.get(x).getDestination() >= floor && pList.get(x).getDestination() >= closest) {
+                    closest = pList.get(x).getDestination();
+                }
+            }
+
+            while(direction==-1)                                    //CKnote - While going down
+            {                                                       //CKnote - Closest should change if destination is between eCar and previous mark
+                if (pList.get(x).getDestination() <= floor && pList.get(x).getDestination() <= closest) {
+                    closest = pList.get(x).getDestination();
+                }
+            }            
+        }
+        return closest;                                             //returns floor floor closest to ecar in terms of direction
+    }
 }
