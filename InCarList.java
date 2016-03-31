df28bf53725df8a5bac9f79a164bf5cce523cc39
Troy <tpastirko@student.cccnj.edu>
2016-03-31 16:59:50 -0400

    Update InCarList.java

--- InCarList.java
+++ InCarList.java
@@ -34,27 +34,36 @@ public class InCarList extends PList
         return false;
     }
 
-    public int checkRequest(int currentFloor, int direction)       //CKnote - Returns the 
+    
+    public int checkUpRequest(int currentFloor)       //CKnote - Returns the 
     {
         int floor = currentFloor;
         int closest = 2000;
         //loops through entire list
         for (int x =0;x<pList.size();x++) {
-
-            while(direction==1)                                     //CKnote - While going up
-            {                                                       //CKnote - Closest should change if destination is between eCar and previous mark
-                if (pList.get(x).getDestination() >= floor && pList.get(x).getDestination() >= closest) {
+                                                    //CKnote - Closest should change if destination is between eCar and previous mark
+                if (pList.get(x).getDestination() >= floor && pList.get(x).getDestination() <= closest) {
                     closest = pList.get(x).getDestination();
                 }
-            }
-
-            while(direction==-1)                                    //CKnote - While going down
-            {                                                       //CKnote - Closest should change if destination is between eCar and previous mark
-                if (pList.get(x).getDestination() <= floor && pList.get(x).getDestination() <= closest) {
-                    closest = pList.get(x).getDestination();
-                }
-            }            
+            
+ 
         }
         return closest;                                             //returns floor floor closest to ecar in terms of direction
+    }
+    
+    public int checkDownRequest(int currentFloor){
+        int floor = currentFloor;
+        int closest = 0;
+        
+        for (int x =0;x<pList.size();x++) {
+
+                                                                //CKnote - Closest should change if destination is between eCar and previous mark
+                if (pList.get(x).getDestination() <= floor && pList.get(x).getDestination() >= closest) {
+                    closest = pList.get(x).getDestination();
+                }
+              
+        }
+        
+        return closest;
     }
 }
