1c06d68b4feb712540088ae96ed8669db4cacad5
ConnorKeenan <whitekeys1234@gmail.com>
2016-04-02 16:12:55 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,1 +1,68 @@
+/**
+ * Creates the initial objects.
+ * Accesses the "Master List(s)" of all the Passengers.
+ * Runs the main simulator loop and a tickCount(i.e. time).
+ * It invokes the act() methods of the main object(ECar and PassengerSource) at each iteration.
+ * 
+ * @author
+ * @February 23, 2016.
+ */
+public class Controller
+{
+    private UpList upList;
+    private DownList downList;
+    private SinkList sinkList;
+    private InCarList incarList;
+    private int maxFloor;
+    private PassengerSource source;
+    private ECar car;
+    private int tick;
+    private Clock myClock;
+    private Statistics myReport; //Connor
+    
+    /**
+     * Constructor for objects of class Controller.
+     */
+    public Controller(int maxFloor, int numRun) throws invalidFloorException //CK- Changed maxCount to numRunand made parameter
+    {
+        if(maxFloor < 2)
+        throw new invalidFloorException();
+        else
+        this.maxFloor = maxFloor;
+        
+        myClock = new Clock();
+        upList = new UpList(myClock);    //sbw added parameters
+        downList = new DownList(myClock);
+        sinkList = new SinkList(myClock);
+        incarList = new InCarList(myClock);  //...sbw
+        myReport = new Statistics(upList,downList,sinkList); //Connor
+    
+        source = new PassengerSource(upList, downList, maxFloor, myClock);
+        car = new ECar(upList, downList, sinkList, incarList, maxFloor, myClock);
+        
+        run(numRun); //CKnote - Changed MaxCount to numRun and set as parameter
+    }
+    
+    /**
+     * Contains the simulation loop to invoke the act method on the objects.
+     */
+    public void run(int numRun) //CKnote - Changed MaxCount to numRun and set as parameter
+    {
+        tick = 0;
+        
+        while (tick < numRun) //CKnote - Changed MaxCount to numRun and set as parameter
+        {
+            source.act();
+            
+            car.act(tick);
+            
+            tick++;
+            
+            myClock.incrementTick();
+            
+        }
+        myReport.fullReport(); //Connor
+    }
+
+}
 
